01697f6ed3a72f5b585511f661b7db7b94d1baf9
MGX <3034181677@qq.com>
2026-07-07 20:45:53 +0800

    blog v1.2

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar:"assets\\images\\微信图片_20260707105852_38_50.avif",
+	avatar:"assets\\images\\touxiang.avif",
 
 	// 名字
 	name: "Magical gold",
--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,11 +12,11 @@ export const siteConfig: SiteConfig = {
 	subtitle: "",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://magical-gold.com",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"热爱二进制的CTFer的小屋",
 
 	// 站点关键词
 	keywords: [
